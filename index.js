3f6ea4a9eeb27bd5e0af4bbb5ff9aa7f674d8037
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-05-18 16:06:53 +0200

    lo

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import config from "./backend/src/config.js";
 import prepareRoutes from "./backend/src/prepareRoutes.js";
 import knex from "knex";
+import BaseModel from "./backend/src/db/models/BaseModel.js";
 
 const db = knex(config.db)
+BaseModel.knex(db); 
 const app = express();
 
 app.use(express.json());
