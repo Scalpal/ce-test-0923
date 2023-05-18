b455daeb11934f03bb99bb239edb0164a4e141b8
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-05-18 16:43:54 +0200

    feat: Add/remove star routes using path variables for session

--- backend/src/db/routes/prepareStarsRoutes.js
+++ backend/src/db/routes/prepareStarsRoutes.js
@@ -1,49 +1,61 @@
 import { integerValidator } from "../../validators.js";
 import validate from "../middlewares/validate.js";
+import SessionModel from "../models/SessionModel.js";
 import SessionReviewModel from "../models/SessionReviewModel.js";
 import UserModel from "../models/UserModel.js";
 
 const prepareStarsRoutes = ({ app, db }) => {
   app.post(
-    "/star/add",
+    "/star/add/:idSession",
     validate({
+      params: { 
+        idSession: integerValidator.required()
+      },
       body: {
         idMentor: integerValidator.required(),
         idStudent: integerValidator.required()
       }
     }),
     async (req, res) => {
+      const idSession = req.params.idSession;
       const idMentor = req.body.idMentor;
       const idStudent = req.body.idStudent; 
 
       try {
+        const session = await SessionModel.query().findOne({ id: idSession })
+
+        if (!session) {
+          res.status(404).send({ error: "Session not found" })
+          return; 
+        }
+
         const mentor = await UserModel.query().findOne({ id: idMentor });
 
         if (!mentor) {
           res.status(404).send({ error: "Mentor not found" })
-
           return; 
         }
 
         if (!mentor.isMentor) {
-          res.status(403).send({ error: "Forbidden" });
-
+          res.status(403).send({ error: "This user is not a mentor." });
           return; 
         }
 
         const student = await UserModel.query().findOne({ id: idStudent });
 
         if (!student) {
-          res.status(404).send({ error: "student not found" })
-
+          res.status(404).send({ error: "Student not found" })
           return; 
         }
 
         const review = await SessionReviewModel.query()
           .select("*")
-          .where("idSession", 1)
-          .andWhere("idMentor", idMentor)
-          .andWhere("idStudent", idStudent)
+          .where("idSession", idSession)
+
+        if (review[0].idMentor !== idMentor || review[0].idStudent !== idStudent) {
+          res.status(403).send({ error: "These users are not related to this session." })
+          return;
+        }
                 
         if (review.length) {
           if (review[0].star === false) {
@@ -51,25 +63,23 @@ const prepareStarsRoutes = ({ app, db }) => {
             .update({ 
               star: true
             })
-            .where("idSession", 1)
-            .andWhere("idMentor", idMentor)
-            .andWhere("idStudent", idStudent)
+            .where("idSession", idSession)
+            .andWhere("idMentor", review[0].idMentor)
+            .andWhere("idStudent", review[0].idStudent)
             
             res.send({ message: "Star updated to the mentor on this session !" });
-
             return;
           }
 
           res.status(409).send({ error: "You already gave a star to your mentor on this session !" })
-
           return;
         }
 
         await SessionReviewModel.query()
           .insert({ 
-            idSession: 1,
-            idMentor: idMentor,
-            idStudent: idStudent,
+            idSession: idSession,
+            idMentor: review[0].idMentor,
+            idStudent: review[0].idStudent,
             star: true
           })
           .returning("*")
@@ -82,45 +92,56 @@ const prepareStarsRoutes = ({ app, db }) => {
   }),
 
   app.delete(
-    "/star/remove",
+    "/star/remove/:idSession",
     validate({
+      params: {
+        idSession: integerValidator.required()
+      },
       body: {
         idMentor: integerValidator.required(),
         idStudent: integerValidator.required()
       }
     }),
     async (req, res) => {
+      const idSession = req.params.idSession; 
       const idMentor = req.body.idMentor;
       const idStudent = req.body.idStudent; 
 
       try {
+        const session = await SessionModel.query().findOne({ id: idSession })
+
+        if (!session) {
+          res.status(404).send({ error: "Session not found" })
+          return; 
+        }
+
         const mentor = await UserModel.query().findOne({ id: idMentor });
 
         if (!mentor) {
           res.status(404).send({ error: "Mentor not found" })
-
           return; 
         }
 
         if (!mentor.isMentor) {
-          res.status(403).send({ error: "Forbidden" });
-
+          res.status(403).send({ error: "This user is not a mentor." });
           return; 
         }
 
         const student = await UserModel.query().findOne({ id: idStudent });
 
         if (!student) {
-          res.status(404).send({ error: "student not found" })
-
+          res.status(404).send({ error: "Student not found" })
           return; 
         }
 
         const review = await SessionReviewModel.query()
           .select("*")
-          .where("idSession", 1)
-          .andWhere("idMentor", idMentor)
-          .andWhere("idStudent", idStudent)
+          .where("idSession", idSession)
+
+        if (review[0].idMentor !== idMentor || review[0].idStudent !== idStudent) {
+          res.status(403).send({ error: "These users are not related to this session." })
+          return;
+        }
                 
         if (review.length) {
           if (review[0].star === true) {
@@ -128,9 +149,9 @@ const prepareStarsRoutes = ({ app, db }) => {
             .update({ 
               star: false
             })
-            .where("idSession", 1)
-            .andWhere("idMentor", idMentor)
-            .andWhere("idStudent", idStudent)
+            .where("idSession", idSession)
+            .andWhere("idMentor", review[0].idMentor)
+            .andWhere("idStudent", review[0].idStudent)
             
             res.send({ message: "Star updated to the mentor on this session !" });
             return;
@@ -142,9 +163,9 @@ const prepareStarsRoutes = ({ app, db }) => {
 
         await SessionReviewModel.query()
           .insert({ 
-            idSession: 1,
-            idMentor: idMentor,
-            idStudent: idStudent,
+            idSession: idSession,
+            idMentor: review[0].idMentor,
+            idStudent: review[0].idStudent,
             star: false
           })
           .returning("*")
